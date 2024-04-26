77bb46121a69fdf27665b4b113c005c9c5033b6e
Diksha <Dikshahanda2345@gmail.com>
2024-04-26 20:50:18 +0530

    THIS IS MY FIRST COMMIT

---
+++ src/app/diksha/diksha.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-diksha',
+  templateUrl: './diksha.component.html',
+  styleUrl: './diksha.component.css'
+})
+export class DikshaComponent {
+
+}
